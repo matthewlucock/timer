543a24cb98035ff8ef3a754555880451e0fee9d5
Matt Lucock <2455682+matthewlucock@users.noreply.github.com>
2021-04-07 15:15:35 +1000

    Prevent transitions firing on page load

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,23 @@
 import * as preact from 'preact'
+import { useEffect } from 'preact/hooks'
 
-import './main.scss'
+import styles from './main.scss'
 
 import { Timer } from './components/timer'
 
+const usePageLoaded = (): void => {
+  useEffect(() => {
+    document.body.classList.add(styles.loaded)
+
+    return () => {
+      document.body.classList.remove(styles.loaded)
+    }
+  }, [])
+}
+
 export const App: preact.FunctionComponent = () => {
+  usePageLoaded()
+
   return (
     <Timer />
   )
